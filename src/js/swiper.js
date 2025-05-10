b7f541270243dbd35534812fea1e3de6f11515b1
Kateryna <katyaukr2014@gmail.com>
2025-05-10 19:38:11 +0300

    add

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -27,5 +27,61 @@ document.addEventListener('DOMContentLoaded', function () {
             prevEl: '.custom-prev',
         },
 
-        });
+    });
+    new Swiper('.reviews-swiper', {
+        modules: [Navigation],
+        breakpoints: {
+            374: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            1439: {
+                slidesPerView: 3,
+                spaceBetween: 24,
+            },
+        },
+        slidesPerView: 'auto',
+        loop: true,
+        slideClass: 'reviews-swiper-slide',
+        wrapperClass: 'reviews-swiper-wrapper',
+        direction: 'horizontal',
+
+        navigation: {
+            nextEl: '.reviews-custom-next',
+            prevEl: '.reviews-custom-prev',
+        },
+
+    });
+    let featuresSwiper = null;
+
+    function initFeaturesSwiper() {
+        const screenWidth = window.innerWidth;
+
+        if (screenWidth < 1439) {
+            if (!featuresSwiper) {
+                featuresSwiper = new Swiper('.features-swiper', {
+                    modules: [Navigation],
+                    slidesPerView: 'auto',
+                    loop: true,
+                    spaceBetween: 20,
+                    slideClass: 'features-swiper-slide',
+                    wrapperClass: 'features-swiper-wrapper',
+                    direction: 'horizontal',
+                    navigation: {
+                        nextEl: '.features-custom-next',
+                        prevEl: '.features-custom-prev',
+                    },
+                });
+            }
+        } else {
+            if (featuresSwiper) {
+                featuresSwiper.destroy(true, true);
+                featuresSwiper = null;
+            }
+        }
+    }
+
+ 
+    initFeaturesSwiper();
+    window.addEventListener('resize', initFeaturesSwiper);
 });
